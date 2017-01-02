42f6da7f2b6da7620bd549c563d344ea47e74851
Felipe Castro <felipegccastro@gmail.com>
2017-01-02 16:06:20 -0200

    Implementação da fila

--- teste.py
+++ teste.py
@@ -1,1 +1,47 @@
-# teste Python
+# coding: latin1
+
+'''
+Autores: Felipe Casto, Gabriel Matos, Josué Pereira e Julia Anne
+Trabalho Final de AD 2016
+'''
+
+class Jobs:
+    pass
+'''classe
+trabalho residual'''
+
+
+
+
+
+
+class Fila:
+    '''Classe definida para organizar a tratar a fila de Jobs'''
+    def __init__(self, scheduling):
+        self.queue = []
+        self.scheduling = scheduling
+        if self.scheduling != "FCFS" and self.scheduling != "LCFS":
+            raise Exception('Escalonamento não definido !!!')    
+
+    def getQueue(self):
+        if self.scheduling == "FCFS":
+            return self.queue.pop(0)
+        elif self.scheduling == "LCFS":
+            return self.queue.pop(len(self.queue)-1)
+
+    def addQueue(self, job):
+        self.queue.append(job)
+
+
+
+
+
+
+
+class Server:
+    pass
+
+
+
+if __name__ == '__main__':
+    fila = Fila("FCFS")
