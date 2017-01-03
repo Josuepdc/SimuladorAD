8a92cb573bdec035243b0b5c0e53efb819d80d19
gabrielmatos26 <matos.gabriel26@gmail.com>
2017-01-03 17:53:35 -0200

    funcionou caralho

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ import random, time
 import utils
 
 if __name__ == '__main__':
-	'''=========================== INICIALIZA��O =========================== '''
+	'''=========================== Parametros iniciais =========================== '''
 	lambda1 = 5
 	lambda2 = 15
 	mu = 20
 	# mu2 = 15
-	T = 0
+	tempoAteProximaChegada = 0
 	trabalhoNoServidor = 0
+	tempoDeSaida = 0
 
 	contadorIds = 0
 	escalonamento = "FCFS"
@@ -22,23 +23,94 @@ if __name__ == '__main__':
 	fila = utils.Fila(escalonamento)
 	servidor = utils.Servidor(mu)
 
+	'''======================= Inicializacao ======================== '''
+	proximoClasse1 = random.expovariate(1/lambda1)
+	tempoAteProximaChegada += proximoClasse1
+	print(50*'-')
+	print("Tempo ate a proxima chegada: ", tempoAteProximaChegada)
+	contadorIds += 1
+	# tempoDeSaida = tempoAteProximaChegada + job.getTempoDeServico()
+	# print("Tempo de saida do Job ", job.getID(), ": ", tempoDeSaida)
+
+
 	while True:
-		proximoClasse1 = random.expovariate(1/lambda1)
-		T += proximoClasse1
-		print("Proxima chegada: ", T)
-		contadorIds += 1
-		fila.addFila(utils.Job(1, random.uniform(0,2), contadorIds))
-		tam = fila.getTamanhoFila()
-		print ("Tamanho da Fila: ", tam)
-		if servidor.getJob() == None:
-			servidor.setJob(fila.getProximoDaFila())
-			job = servidor.getJob()
-			tempoJob = job.getTempoDeServico()
-			trabalhoNoServidor = tempoJob
-			print("Tempo de servico: ", tempoJob)
-			print("Classe: ", job.getClasse())
+		# if tempoDeSaida > 0:
+		if tempoAteProximaChegada < tempoDeSaida:
+			fila.addFila(utils.Job(1, random.uniform(10,30), contadorIds))
+			print("Job ", contadorIds, " entrou na fila")
+			tam = fila.getTamanhoFila()
+			print ("Tamanho da Fila: ", tam)
 		else:
-			trabalhoNoServidor -= taxa
+			if tempoDeSaida != 0:
+				print("Job ", servidor.getJob().getID(), " terminou")
+				servidor.setJob(None)
+				if fila.getTamanhoFila() != 0:
+					servidor.setJob(fila.getProximoDaFila())
+					job = servidor.getJob()
+					print("Job ", job.getID(), " entrou em servico")
+					tempoJob = job.getTempoDeServico()
+					trabalhoNoServidor = tempoJob
+					tam = fila.getTamanhoFila()
+					print("Tempo de servico: ", tempoJob)
+					print("Classe: ", job.getClasse())
+				print ("Tamanho da Fila: ", tam)
+				if tam < 1:
+					tempoDeSaida = tempoAteProximaChegada + tempoJob
+					print("Tempo de saida do Job ", job.getID(), ": ", tempoDeSaida)
+				else:
+					tempoDeSaida += tempoJob
+					print("Tempo de saida do Job ", job.getID(), ": ", tempoDeSaida)
+			else:
+				job = utils.Job(1, random.uniform(10,30), contadorIds)
+				fila.addFila(job)
+				print("Job ", contadorIds, " entrou na fila")
+				servidor.setJob(fila.getProximoDaFila())
+				job = servidor.getJob()
+				print("Job ", job.getID(), " entrou em servico")
+				tempoJob = job.getTempoDeServico()
+				trabalhoNoServidor = tempoJob
+				tam = fila.getTamanhoFila()
+				print("Tempo de servico: ", tempoJob)
+				print("Classe: ", job.getClasse())
+				tempoDeSaida = tempoAteProximaChegada + tempoJob
+				print("Tempo de saida do Job ", job.getID(), ": ", tempoDeSaida)
+
+		proximoClasse1 = random.expovariate(1/lambda1)
+		tempoAteProximaChegada += proximoClasse1
+		print(50*'-')
+		print("Tempo ate a proxima chegada: ", tempoAteProximaChegada)
+		contadorIds += 1
+		# else:
+		# 	fila.addFila(utils.Job(1, random.uniform(10,30), contadorIds))
+		# 	servidor.setJob(fila.getProximoDaFila())
+		# 	job = servidor.getJob()
+		# 	tempoJob = job.getTempoDeServico()
+		# 	trabalhoNoServidor = tempoJob
+		# 	tam = fila.getTamanhoFila()
+		# 	print("Tempo de servico: ", tempoJob)
+		# 	print("Classe: ", job.getClasse())
+		# 	print ("Tamanho da Fila: ", tam)
+
+
+
+
+
+		# tam = fila.getTamanhoFila()
+		# print ("Tamanho da Fila: ", tam)
+		# if servidor.getJob() == None:
+		# 	servidor.setJob(fila.getProximoDaFila())
+		# 	job = servidor.getJob()
+		# 	tempoJob = job.getTempoDeServico()
+		# 	trabalhoNoServidor = tempoJob
+		# 	tam = fila.getTamanhoFila()
+		# 	print("Tempo de servico: ", tempoJob)
+		# 	print("Classe: ", job.getClasse())
+		# 	if tam < 1:
+		# 		tempoDeSaida = tempoAteProximaChegada + tempoJob
+		# 		print(tempoDeSaida)
+		# 	else:
+		# 		tempoDeSaida += tempoJob
+
 	# proximoClasse2 = random.expovariate(1/lambda2)
 
 
